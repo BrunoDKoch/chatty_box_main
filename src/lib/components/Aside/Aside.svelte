c505be79759cf11feb2c0485f2678d6aa62d01b6
BrunoDKoch <bruno.d.koch@gmail.com>
2023-04-23 09:16:28 -0300

    Add settings component

--- src/lib/components/Aside/Aside.svelte
+++ src/lib/components/Aside/Aside.svelte
@@ -1,31 +1,46 @@
 <script lang="ts">
+  import SettingsComponent from '../Settings/SettingsComponent.svelte';
   import AsideMsg from './AsidePreview.svelte';
   import type { ChatPreview } from '$lib/types/partialTypes';
   import AsideSearch from './AsideSearch.svelte';
   import { t } from 'svelte-i18n';
   import useActiveScreen from '$lib/useActiveScreen';
+  import { connection } from '$lib/useSignalR';
+  import { onMount } from 'svelte';
   export let chats: ChatPreview[];
+  let settingsMenuActive = false;
+  let userInfo: { userName: string; avatar?: string };
+  connection.on('userInfo', (data: typeof userInfo) => (userInfo = data));
+  onMount(async () => await connection.invoke('GetCallerInfo'));
 </script>
 
 <div class="flex flex-col gap-3">
   <div class="flex items-center">
-    <button class="btn btn-ghost">
+    <button on:click={() => (settingsMenuActive = !settingsMenuActive)} class="btn btn-ghost">
       <iconify-icon icon="mdi:menu" height="1.4rem" />
     </button>
-    <AsideSearch />
+    {#if settingsMenuActive}
+      <h2 class="capitalize">{$t('common.settings')}</h2>
+    {:else}
+      <AsideSearch />
+    {/if}
   </div>
-  <div
-    on:keydown={() => ($useActiveScreen = 'friends')}
-    on:click={() => ($useActiveScreen = 'friends')}
-    class="flex py-2 gap-3 items-center cursor-pointer hover:bg-gray-300 hover:dark:bg-gray-700"
-  >
-    <iconify-icon icon="material-symbols:person" />
-    <p class="capitalize">{$t('common.friend')}s</p>
-  </div>
-  <div class="divider" />
-  <div class="flex flex-col even:bg-base-300">
-    {#each chats as chat}
-      <AsideMsg {chat} />
-    {/each}
-  </div>
+  {#if settingsMenuActive}
+    <SettingsComponent user={userInfo} />
+  {:else}
+    <div
+      on:keydown={() => ($useActiveScreen = 'friends')}
+      on:click={() => ($useActiveScreen = 'friends')}
+      class="flex py-2 gap-3 items-center cursor-pointer hover:bg-gray-300 hover:dark:bg-gray-700"
+    >
+      <iconify-icon icon="material-symbols:person" />
+      <p class="capitalize">{$t('common.friend')}s</p>
+    </div>
+    <div class="divider" />
+    <div class="flex flex-col even:bg-base-300">
+      {#each chats as chat}
+        <AsideMsg {chat} />
+      {/each}
+    </div>
+  {/if}
 </div>
---
+++ src/lib/components/Settings/SettingsComponent.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { t } from 'svelte-i18n';
+
+  export let user: { userName: string; avatar?: string };
+  let activeScreen: 'main' | 'privacy' = 'main';
+  const settingsItems = [
+    $t('settings.privacy'),
+    $t('settings.security'),
+    $t('settings.notifications'),
+    $t('settings.language'),
+    $t('settings.darkTheme'),
+  ];
+</script>
+
+<div class="flex">
+  <figure class="avatar mask w-24 h-24 mask-squircle">
+    {#if user.avatar}
+      <img src={user.avatar} alt="" />
+    {:else}
+      <div
+        class="flex items-center justify-center bg-blue-600 text-white dark:bg-blue-800 h-full"
+      >
+        <p class="text-center font-bold py-2 text-7xl">{user.userName[0]}</p>
+      </div>
+    {/if}
+  </figure>
+  <p class="font-bold text-3xl">{user.userName}</p>
+</div>
+<menu class="menu">
+  {#each settingsItems as item}
+    <li class="uppercase">
+      <a href="/" on:click|preventDefault>{item}</a>
+    </li>
+  {/each}
+</menu>
