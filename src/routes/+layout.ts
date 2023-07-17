33674c5e8946cf30e335b8d783605eedc6d952c7
BrunoDKoch <bruno.d.koch@gmail.com>
2023-07-17 11:15:04 -0300

    Improve layout load

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,11 +3,9 @@ import '$lib/i18n';
 import { locale, waitLocale } from 'svelte-i18n';
 import type { LayoutLoad } from './$types';
 
-export const ssr = false;
-export const csr = true;
-
 export const load = (async (event) => {
-  if (event.data.lang) locale.set(event.data.lang.slice(0, 2));
+  const { lang } = event.data;
+  if (lang) locale.set(lang.slice(0, 2));
   else if (browser) {
     locale.set(window.navigator.language.slice(0, 2));
   }
